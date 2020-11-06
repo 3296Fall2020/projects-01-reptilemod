1785303947137e21bcd6f26289cff69fb7867878
Christopher-Scott <44304416+Christopher-Scott@users.noreply.github.com>
2020-11-06 16:42:23 -0500

    Converted Crocodile to monster entity, added targeting

--- src/main/java/edu/temple/reptiles/entities/CrocodileEntity.java
+++ src/main/java/edu/temple/reptiles/entities/CrocodileEntity.java
@@ -1,39 +1,44 @@
 package edu.temple.reptiles.entities;
 
-import edu.temple.reptiles.init.ModEntityTypes;
-import net.minecraft.entity.AgeableEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.*;
-import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.passive.ChickenEntity;
+import net.minecraft.entity.passive.CowEntity;
+import net.minecraft.entity.passive.PigEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
-public class CrocodileEntity extends AnimalEntity {
+public class CrocodileEntity extends MonsterEntity {
 
-    public CrocodileEntity(EntityType<? extends AnimalEntity> type, World worldIn){
+    public CrocodileEntity(EntityType<? extends MonsterEntity> type, World worldIn){
         super(type, worldIn);
     }
 
     public static AttributeModifierMap.MutableAttribute setCustomAttributes(){
-        return AnimalEntity.func_233666_p_()
-                .createMutableAttribute(Attributes.MAX_HEALTH, 10.0D)
-                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.2D);
+        return MonsterEntity.func_234295_eP_()
+                .createMutableAttribute(Attributes.MAX_HEALTH, 20.0D)
+                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.2D)
+                .createMutableAttribute(Attributes.ATTACK_DAMAGE, 4.0D);
     }
 
-    public AgeableEntity createChild(AgeableEntity ageable){
-        return ModEntityTypes.CROCODILE.get().create(this.world);
-    }
 
     @Override
     protected void registerGoals(){
         super.registerGoals();
         this.goalSelector.addGoal(0, new SwimGoal(this));
-        this.goalSelector.addGoal(1, new PanicGoal(this, 1.25D));
-        this.goalSelector.addGoal(5, new RandomWalkingGoal(this, 1.0D));
-        this.goalSelector.addGoal(6, new LookAtGoal(this, PlayerEntity.class, 6.0F));
-        this.goalSelector.addGoal(7, new LookRandomlyGoal(this));
+        this.goalSelector.addGoal(4, new MeleeAttackGoal(this, 1.2D, false));
+        this.goalSelector.addGoal(8, new RandomWalkingGoal(this, 1.0D));
+        this.goalSelector.addGoal(8, new LookAtGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
+
+        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
+        this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, CowEntity.class, true));
+        this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, PigEntity.class, true));
+        this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, ChickenEntity.class, true));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
 
     }
 
@@ -42,8 +47,5 @@ public class CrocodileEntity extends AnimalEntity {
         return 1 + this.world.rand.nextInt(4);
     }
 
-    @Override
-    public void livingTick(){
-        super.livingTick();
-    }
+
 }
