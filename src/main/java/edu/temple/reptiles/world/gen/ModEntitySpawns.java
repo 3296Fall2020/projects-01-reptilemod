1cbf5bf0dd099375c1700fba62ca4db7968633b5
Jake <tuh13091@temple.edu>
2020-11-16 15:10:28 -0500

    Bearded dragon natural spawns working

--- src/main/java/edu/temple/reptiles/world/gen/ModEntitySpawns.java
+++ src/main/java/edu/temple/reptiles/world/gen/ModEntitySpawns.java
@@ -20,6 +20,9 @@ public class ModEntitySpawns {
             if (biome.getCategory() == Biome.Category.DESERT || biome.getCategory() == Biome.Category.PLAINS || biome.getCategory() == Biome.Category.SAVANNA || biome.getCategory() == Biome.Category.MESA){
                 biome.getSpawns(EntityClassification.MONSTER).add(new Biome.SpawnListEntry(ModEntityTypes.DINOSAUR.get(), 100, 1, 6));
             }
+            if(biome.getCategory() == Biome.Category.DESERT || biome.getCategory() == Biome.Category.MESA){
+                biome.getSpawns(EntityClassification.CREATURE).add(new Biome.SpawnListEntry(ModEntityTypes.BEARDEDDRAGON.get(), 100, 1, 2));
+            }
         }
     }
 
