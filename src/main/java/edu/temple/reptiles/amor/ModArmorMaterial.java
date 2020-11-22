76175d05c7602315b0cc616b0acdaa87cbac1d66
Cameron <tuh30695@temple.edu>
2020-11-22 16:14:15 -0500

    Scales and Scale mail armor added to the game.  Currently the armor is OP, is uncraftable, and has no texture.

---
+++ src/main/java/edu/temple/reptiles/amor/ModArmorMaterial.java
@@ -0,0 +1,84 @@
+package edu.temple.reptiles.amor;
+
+import edu.temple.reptiles.Reptiles;
+import edu.temple.reptiles.init.ModItems;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.function.Supplier;
+
+public enum ModArmorMaterial implements IArmorMaterial {
+
+    // scale_layer_1 scale_layer_2
+    SCALE(Reptiles.MOD_ID + ":scale", 24, new int[] {3, 5, 6, 3}, 14,
+            SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 0.5f,
+            () -> { return Ingredient.fromItems(ModItems.SCALE.get());}, 1.0f);
+
+    private static final int[] MAX_DAMAGE_ARRAY = new int[] {11, 16, 15, 13};
+    private final String name;
+    private final int maxDamageFactor;
+    private final int[] damageReductionAmountArray;
+    private final int enchantability;
+    private final SoundEvent soundEvent;
+    private final float toughness;
+    private final Supplier<Ingredient> repairMaterial;
+    private final float knockbackResistance;
+
+    ModArmorMaterial(String name, int maxDamageFactor, int[] damageReductionAmountArray, int enchantability,
+                     SoundEvent soundEvent, float toughness, Supplier<Ingredient> repairMaterial, float knockbackResistance) {
+        this.name = name;
+        this.maxDamageFactor = maxDamageFactor;
+        this.damageReductionAmountArray = damageReductionAmountArray;
+        this.enchantability = enchantability;
+        this.soundEvent = soundEvent;
+        this.toughness = toughness;
+        this.repairMaterial = repairMaterial;
+        this.knockbackResistance = knockbackResistance;
+    }
+
+    @Override
+    public int getDurability(EquipmentSlotType slotIn) {
+        return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.maxDamageFactor;
+    }
+
+    @Override
+    public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+        return this.damageReductionAmountArray[slotIn.getIndex()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public SoundEvent getSoundEvent() {
+        return this.soundEvent;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return this.repairMaterial.get();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
